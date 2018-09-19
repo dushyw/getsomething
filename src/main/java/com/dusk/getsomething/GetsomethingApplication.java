8fc2f21d1d2fd9bd51b1e8ef0bae32935e57c5e2
运维服务部-袁伟\user <1215765064@qq.com>
2018-09-19 11:22:34 +0800

    初始框架

---
+++ src/main/java/com/dusk/getsomething/GetsomethingApplication.java
@@ -0,0 +1,12 @@
+package com.dusk.getsomething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetsomethingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetsomethingApplication.class, args);
+    }
+}
