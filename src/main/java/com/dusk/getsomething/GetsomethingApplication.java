61cddaab9e9d162990ae8b1826028e3d4ef52499
运维服务部-袁伟\user <1215765064@qq.com>
2018-09-20 16:20:43 +0800

    添加mysql部分设置

--- src/main/java/com/dusk/getsomething/GetsomethingApplication.java
+++ src/main/java/com/dusk/getsomething/GetsomethingApplication.java
@@ -1,5 +1,6 @@
 package com.dusk.getsomething;
 
+import com.dusk.getsomething.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/dusk/getsomething/config/Config.java
@@ -0,0 +1,21 @@
+package com.dusk.getsomething.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@ConfigurationProperties
+public class Config {
+    private List<String> list = new ArrayList<>();
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
